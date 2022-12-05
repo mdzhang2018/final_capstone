ecca035343bde18f4009aee2f07f3584404b0ac9
Zhang <mdzhang@b0be836f5444.ant.amazon.com>
2022-12-05 13:09:34 -0800

    changed front-end Amazonian to be AWSome Speaker Analytics

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ const App = ({ signOut }) => {
 
   return (
     <View className="App">
-      <Heading level={1}>AWS Speaker Analytics</Heading>
+      <Heading level={1}>AWSome Speaker Analytics</Heading>
       <View as="form" margin="3rem 0" onSubmit={createNote}>
         <Flex direction="row" justifyContent="center">
           <TextField
